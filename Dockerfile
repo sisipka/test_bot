09491db296228fed04d05cd0aeac91e81a6a9ed4
Andrew Shestihin <shestihinandrey@gmail.com>
2023-07-24 11:00:43 +0300

    beginning

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:alpine
+
+# Установим директорию для работы
+WORKDIR /telegram_bot
+
+# Копируем зависимости
+COPY ./requirements.txt .
+
+# Устанавливаем зависимости
+RUN pip install --upgrade pip
+RUN pip install --no-cache-dir -r ./requirements.txt
+
+# Копируем проект
+
+COPY ./bot.py .
+RUN chmod -R 777 ./bot.py
+
+# run app
+CMD ["python", "bot.py"]
---
+++ bot.py
@@ -0,0 +1,37 @@
+from requests import get
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+
+# ip = get('https://ipapi.co/ip/').text
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="6421437309:AAH1pls2rdmF5K-pcnv771svphiNUBRDJ0Y")
+# Диспетчер
+dp = Dispatcher(bot)
+
+# Хэндлер на команду /start
+@dp.message_handler(commands=['start', 'help']) 
+async def send_welcome(msg: types.Message):
+    await msg.answer(f'Я бот. Приятно познакомиться, {msg.from_user.first_name}')
+
+@dp.message_handler(content_types=['text']) 
+async def get_text_messages(msg: types.Message):
+   if msg.text.lower() == 'привет': 
+       await msg.answer('Привет!')
+   elif msg.text.lower() == 'myip':
+       await msg.answer(get('https://api.ipify.org').text)
+   else:
+       await msg.answer('Не понимаю, что это значит.')
+
+
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
