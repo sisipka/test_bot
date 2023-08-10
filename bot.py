f3c96348334eddb3048ed2d470be56044fb7bedc
Andrew Shestihin <shestihinandrey@gmail.com>
2023-08-10 17:03:33 +0300

    add /help

--- bot.py
+++ bot.py
@@ -3,30 +3,39 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 
-# ip = get('https://ipapi.co/ip/').text
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
+
 # Объект бота
-bot = Bot(token="TOKEN") # TOKEN tellegramm bot
+bot = Bot(token="TOKEN")  # TOKEN tellegramm bot
+
 # Диспетчер
 dp = Dispatcher(bot)
 
 # Хэндлер на команду /start
-@dp.message_handler(commands=['start', 'help']) 
+
+
+@dp.message_handler(commands=['start'])
 async def send_welcome(msg: types.Message):
     await msg.answer(f'Я бот. Приятно познакомиться, {msg.from_user.first_name}')
 
-@dp.message_handler(content_types=['text']) 
+# Хэндлер на команду /help
+
+
+@dp.message_handler(commands=['help'])
+async def send_welcome(msg: types.Message):
+    await msg.answer('Я бот. Создан для тестов и экспериментов')
+
+
+@dp.message_handler(content_types=['text'])
 async def get_text_messages(msg: types.Message):
-   if msg.text.lower() == 'привет': 
-       await msg.answer('Привет!')
-   elif msg.text.lower() == 'myip':
-       await msg.answer(get('https://api.ipify.org').text)
-   else:
-       await msg.answer('Не понимаю, что это значит.')
-
-
+    if msg.text.lower() == 'привет':
+        await msg.answer('Привет!')
+    elif msg.text.lower() == 'myip':
+        await msg.answer(get('https://api.ipify.org').text)
+    else:
+        await msg.answer('Не понимаю, что это значит.')
 
 
 # Запуск процесса поллинга новых апдейтов
