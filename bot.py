8653636e8e55a077501d122a2de71e90a6a42893
Andrew Shestihin <shestihinandrey@gmail.com>
2023-08-15 17:41:34 +0300

    update

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from requests import get
 import asyncio
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, executor, types, utils
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -50,6 +50,29 @@ async def get_text_messages(msg: types.Message):
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
+
+# Местоположение пользователя
+
+
+def get_keyboard():
+    keyboard = types.ReplyKeyboardMarkup()
+    button = types.KeyboardButton("Share Position", request_location=True)
+    keyboard.add(button)
+    return keyboard
+
+
+@dp.message_handler(content_types=['location'])
+async def handle_location(message: types.Message):
+    lat = message.location.latitude
+    lon = message.location.longitude
+    reply = "latitude:  {}nlongitude: {}".format(lat, lon)
+    await message.answer(reply, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(commands=['locate_me'])
+async def cmd_locate_me(message: types.Message):
+    reply = "Click on the the button below to share your location"
+    await message.answer(reply, reply_markup=get_keyboard())
 
 if __name__ == "__main__":
     asyncio.run(main())
