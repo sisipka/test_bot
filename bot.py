22d1fa5dbb85bce66232489e9088a2ae3d79046a
Andrew Shestihin <shestihinandrey@gmail.com>
2023-08-15 12:29:13 +0300

    update

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 
-from geolocation import GPS
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -46,15 +45,6 @@ async def get_text_messages(msg: types.Message):
         await msg.answer(get('https://api.ipify.org').text)
     else:
         await msg.answer('Не понимаю, что это значит.')
-
-# Функция для получения местоположения
-
-
-@bot.message_handler(commands=['geo'])
-def geolocation(message):
-    # Получаем местоположение пользователя из ответа сервера
-    user_location = message.text.split(',')[1].strip()
-    await msg.answer(user_location.text)
 
 
 # Запуск процесса поллинга новых апдейтов
