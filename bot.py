9a4d3ddaae26d6ed5edb389bd192bb09d7cb46be
Andrew Shestihin <shestihinandrey@gmail.com>
2023-08-17 17:36:37 +0300

    update

--- bot.py
+++ bot.py
@@ -51,16 +51,6 @@ async def get_text_messages(msg: types.Message):
 # test
 
 
-# Если не указать фильтр F.text,
-# то хэндлер сработает даже на картинку с подписью /test,
-# но пока нам это не важно и рассматриваем только текстовые сообщения
-
-@dp.message(Command("test"))
-async def any_message(message: types.Message):
-    await message.answer("Hello, <b>world</b>!", parse_mode="HTML")
-    await message.answer("Hello, *world*\!", parse_mode="MarkdownV2")
-
-
 # Запуск процесса поллинга новых апдейтов
 
 
