3b8e28b7a4b9be69a5208c757e4ee90f695190ac
Andrew Shestihin <shestihinandrey@gmail.com>
2023-09-11 14:04:09 +0300

    update

--- bot.py
+++ bot.py
@@ -14,6 +14,25 @@ bot = Bot(token="TOKEN", parse_mode="HTML")  # TOKEN tellegramm bot
 # Диспетчер
 dp = Dispatcher(bot)
 
+# Меню
+
+
+# Создаем асинхронную функцию
+async def set_main_menu(bot: Bot):
+
+    # Создаем список с командами и их описанием для кнопки menu
+    main_menu_commands = [
+        BotCommand(command='/help',
+                   description='Справка по работе бота'),
+        BotCommand(command='/support',
+                   description='Поддержка'),
+        BotCommand(command='/contacts',
+                   description='Другие способы связи'),
+        BotCommand(command='/payments',
+                   description='Платежи')]
+
+    await bot.set_my_commands(main_menu_commands)
+
 # Кнопки
 
 b1 = KeyboardButton('/start')
