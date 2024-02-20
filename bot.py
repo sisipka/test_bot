8e8ebae8db264e25ee99ccc5ef4b84e60d2570ae
Andrew Shestihin <shestihinandrey@gmail.com>
2024-02-20 20:08:27 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, BotCommand
+from aiogram.filters import Command
 
 
 # Включаем логирование, чтобы не пропустить важные сообщения
