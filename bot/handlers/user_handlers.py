af3a01c97a25bb480eb1f876bc361bc3da2f08b6
Andrew Shestihin <shestihinandrey@gmail.com>
2024-03-04 20:28:37 +0300

    update

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -4,7 +4,6 @@ from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import CallbackQuery, Message
 from database.database import user_dict_template, users_db
-from keyboards.pagination_kb import create_pagination_keyboard
 from lexicon.lexicon import LEXICON
 from services.file_handling import book
 
