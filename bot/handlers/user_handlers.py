b472a3aac38c77e449bf844fe5afb70a4cd6bb72
Andrew Shestihin <shestihinandrey@gmail.com>
2024-03-04 20:25:08 +0300

    update

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -4,9 +4,6 @@ from aiogram import F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import CallbackQuery, Message
 from database.database import user_dict_template, users_db
-from filters.filters import IsDelBookmarkCallbackData, IsDigitCallbackData
-from keyboards.bookmarks_kb import (create_bookmarks_keyboard,
-                                    create_edit_keyboard)
 from keyboards.pagination_kb import create_pagination_keyboard
 from lexicon.lexicon import LEXICON
 from services.file_handling import book
--- bot/keyboards/bookmarks_kb.py
+++ bot/keyboards/bookmarks_kb.py
@@ -1,7 +1,6 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from lexicon.lexicon import LEXICON
-from services.file_handling import book
 
 
 def create_bookmarks_keyboard(*args: int) -> InlineKeyboardMarkup:
