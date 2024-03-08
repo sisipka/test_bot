aaf1d927ca522a28c69c37e6bff6df0667d1077d
Andrew Shestihin <shestihinandrey@gmail.com>
2024-03-08 22:11:40 +0300

    update

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -14,7 +14,7 @@ router = Router()
 # и отправлять ему приветственное сообщение
 @router.message(CommandStart())
 async def process_start_command(message: Message):
-    await message.answer("Hello!")
+    await message.answer(LEXICON[message.text])
 
 
 @router.message(Command(commands='help'))
